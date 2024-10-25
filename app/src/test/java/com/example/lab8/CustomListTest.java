f29463bcc9a867cfa39157eab2bcad8145003e1f
MacKenzie Luong <luongmackenzie@gmail.com>
2024-10-25 12:34:51 -0600

    hasCity Test

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -33,6 +33,25 @@ public class CustomListTest {
         list.addCity(new City("Estevan", "SK"));
         assertEquals(list.getCount(),listSize + 1);
     }
+    /**
+     * get the size of the list
+     * increase the list by adding a new city
+     * check if our list contains the new city
+     */
+    @Test
+    public void hasCityTest(){
+        list = MockCityList();
+// This line checks if the first city in the cityList (retrieved by cityList.getCities().get(0))
+// is the same as the city returned by mockCity()
+        assertEquals(0, MockCityList().compareTo(list.getCities().get(0)));
+// This pushes down the original city
+        City city = new City("Charlottetown", "Prince Edward Island");
+        list.add(city);
+// Now the original city should be at position 1
+        assertEquals(0, city.compareTo(list.getCities().get(0)));
+        assertEquals(0, MockCityList().compareTo(list.getCities().get(1)));
+    }
+    }
 
 
 
